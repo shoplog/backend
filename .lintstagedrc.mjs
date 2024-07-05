086911d5afffc30c8ee482a55f2357bba7d3e204
Albin Ma <albinma@gmail.com>
2024-07-04 23:35:43 -0400

    chore: enforce format and commit msg

---
+++ .lintstagedrc.mjs
@@ -0,0 +1,15 @@
+const Configuration = {
+	// Type check TypeScript files
+	'**/*.(ts)': () => 'pnpm tsc --noEmit',
+
+	// Lint & Prettify TS and JS files
+	'**/*.(js|ts)': (filenames) => [
+		`pnpm eslint ${filenames.join(' ')}`,
+		`pnpm prettier --write ${filenames.join(' ')} --plugin-search-dir=.`,
+	],
+
+	// Prettify only Markdown and JSON files
+	'**/*.(md|json)': (filenames) => `yarn prettier --write ${filenames.join(' ')} --plugin-search-dir=.`,
+};
+
+export default Configuration;
