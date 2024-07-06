97acaa7185d680c969d2bdf11bc894ddc8d372ed
Albin Ma <albinma@gmail.com>
2024-07-06 00:02:13 -0400

    feat: add helmet and other headers

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,24 @@
-import express from 'express';
+import compression from 'compression';
+import express, { json } from 'express';
+import helmet from 'helmet';
 import pinoHttp from 'pino-http';
 import { CUSTOM_HEADERS } from 'src/common/constants/headers';
 import { logger } from 'src/initializers/logger';
 
 export const setupApp = () => {
 	const app = express();
+
+	app.use(
+		helmet({
+			// Disable CSP because it's not needed for this API only project.
+			contentSecurityPolicy: false,
+		})
+	);
+
+	// For when the app is behind a proxy, like in a docker container or in a kubernetes cluster.
+	app.set('trust proxy', 1);
+
+	app.use(compression());
 
 	// Generate request id that will correlate all logs for a single request.
 	app.use((req, res, next) => {
@@ -20,6 +34,8 @@ export const setupApp = () => {
 			genReqId: (req) => req.id,
 		})
 	);
+
+	app.use(json());
 
 	app.get('/', (req, res) => {
 		res.status(200).send('Ok!');
--- tests/integrations/index.test.ts
+++ tests/integrations/index.test.ts
@@ -27,4 +27,23 @@ describe('GET /', () => {
 		// assert
 		expect(response).not.toBeNil();
 	});
+
+	it('should have security headers', async () => {
+		const headers = await supertest(app)
+			.get('/')
+			.expect(200)
+			.then((res) => res.headers);
+
+		expect(headers).not.toBeNil();
+		expect(headers['cross-origin-opener-policy']).toBe('same-origin');
+		expect(headers['cross-origin-resource-policy']).toBe('same-origin');
+		expect(headers['referrer-policy']).toBe('no-referrer');
+		expect(headers['x-content-type-options']).toBe('nosniff');
+		expect(headers['x-dns-prefetch-control']).toBe('off');
+		expect(headers['x-frame-options']).toBe('SAMEORIGIN');
+		expect(headers['x-xss-protection']).toBe('0');
+		expect(headers['x-download-options']).toBe('noopen');
+		expect(headers['x-permitted-cross-domain-policies']).toBe('none');
+		expect(headers['x-powered-by']).toBeUndefined();
+	});
 });
