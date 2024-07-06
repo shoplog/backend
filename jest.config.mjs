74b413fc72650d3cdad88661496d340183664fe7
Albin Ma <albinma@gmail.com>
2024-07-05 23:59:20 -0400

    chore: refactor tests

--- jest.config.mjs
+++ jest.config.mjs
@@ -3,6 +3,7 @@ const config = {
 		'^.+\\.ts$': '@swc/jest',
 	},
 	runtime: '@side/jest-runtime',
+	setupFilesAfterEnv: ['jest-extended/all'],
 };
 
 export default config;
--- tests/integrations/index.test.ts
+++ tests/integrations/index.test.ts
@@ -1,19 +1,30 @@
 import { setupApp } from 'src/routes';
+import { Express } from 'express';
 import supertest from 'supertest';
+import { CUSTOM_HEADERS } from 'src/common/constants/headers';
 
-describe('Main app start', () => {
-	it('should start the app', async () => {
-		// Arrange
-		const app = setupApp();
-		const request = supertest(app);
+describe('GET /', () => {
+	let app: Express;
 
-		// Act
-		const response = await request
-			.get('/')
-			.expect(200)
-			.then((res) => res.text);
+	beforeAll(() => {
+		app = setupApp();
+	});
+
+	it('should return 200 OK', async () => {
+		const response = await supertest(app).get('/');
 
 		// Assert
-		expect(response).toEqual('Ok!');
+		expect(response.status).toBe(200);
+	});
+
+	it('should have request id', async () => {
+		// act
+		const response = await supertest(app)
+			.get('/')
+			.expect(200)
+			.then((res) => res.headers[CUSTOM_HEADERS.RequestId]);
+
+		// assert
+		expect(response).not.toBeNil();
 	});
 });
