643744a4bc7c0a884180175bd1d88495a155bcbe
Albin Ma <albinma@gmail.com>
2024-07-06 18:36:15 -0400

    feat: add context-wrap middleware

---
+++ src/common/middlewares/contex-wrap.middleware.ts
@@ -0,0 +1,37 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { NextFunction, Request, Response } from 'express';
+import { ParamsDictionary, Query } from 'express-serve-static-core';
+
+export type ExpressMiddlewareFunction<
+	P = ParamsDictionary,
+	ResBody = any,
+	ReqBody = any,
+	ReqQuery = Query,
+	Locals extends Record<string, any> = Record<string, any>,
+> = (req: Request<P, ResBody, ReqBody, ReqQuery, Locals>, res: Response<ResBody, Locals>) => Promise<void>;
+
+export function contextWrapMiddleware<
+	P = ParamsDictionary,
+	ResBody = any,
+	ReqBody = any,
+	ReqQuery = Query,
+	Locals extends Record<string, any> = Record<string, any>,
+>(
+	fn: ExpressMiddlewareFunction<P, ResBody, ReqBody, ReqQuery, Locals>
+): (
+	req: Request<P, ResBody, ReqBody, ReqQuery, Locals>,
+	res: Response<ResBody, Locals>,
+	next: NextFunction
+) => Promise<void> {
+	return async (req, res, next) => {
+		try {
+			await fn(req, res);
+
+			if (!res.headersSent) {
+				next();
+			}
+		} catch (error) {
+			next(error);
+		}
+	};
+}
