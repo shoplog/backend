cccac1b9672ad248f2f8b07946c33186bca8e030
Albin Ma <albinma@gmail.com>
2024-07-10 22:35:52 -0400

    feat: change sql-server version and init script

---
+++ data/mssql/scripts/init.sql
@@ -0,0 +1,12 @@
+IF NOT EXISTS(SELECT * FROM sys.databases WHERE name = 'vPICList_Lite1')
+BEGIN
+  CREATE DATABASE [vPICList_Lite1]
+
+  RESTORE DATABASE [vPICList_Lite1]
+  FROM  DISK = N'/var/backups/vPICList_lite1.bak'
+  WITH  FILE = 1,
+  MOVE N'vPICList_Lite1' TO N'/var/opt/mssql/data/vPICList_Lite1.mdf',
+  MOVE N'vPICList_Lite1_log' TO N'/var/opt/mssql/data/vPICList_Lite1_log.ldf',
+  NOUNLOAD,  REPLACE,  STATS = 5
+END
+GO
