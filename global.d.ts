0a5a197c19db5664382711a675f5709663df7df4
Albin Ma <albinma@gmail.com>
2024-08-11 11:23:13 -0400

    chore: remove unused dependencies and code

--- global.d.ts
+++ global.d.ts
@@ -1,13 +1,1 @@
-import { PrismaClient } from '@prisma/client';
 import 'jest-extended';
-
-declare global {
-	namespace Express {
-		interface Request {
-			db: Omit<PrismaClient, '$connect' | '$disconnect' | '$on' | '$transaction' | '$use' | '$extends'>;
-			user?: {
-				id: string;
-			};
-		}
-	}
-}
